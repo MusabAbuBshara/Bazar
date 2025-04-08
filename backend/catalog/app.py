c3e6ea144ca6f1e8f4d8623bae519bce4e7e2145
MusabAbuBshara <musababubshara@gmail.com>
2025-04-08 10:28:04 +0300

    Endpoint to get books by topic

--- backend/catalog/app.py
+++ backend/catalog/app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask , jsonify
 import sqlite3
 
-// Setting up the database path for the catalog service in data folder.
+# Setting up the database path for the catalog service in data folder.
 app = Flask(__name__)
 DATABASE = '/data/catalog.db'
 
-// Establishing a connection to an SQLite database to store the catalog data.
+# Establishing a connection to an SQLite database to store the catalog data.
 def get_db():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
+
+# Response to return books by topic to the frontend request.
+@app.route('/search/<topic>', methods=['GET'])
+def search(topic):
+    conn = get_db()
+    books = conn.execute('SELECT id, title FROM books WHERE topic = ?', (topic,)).fetchall()
+    conn.close()
+    return jsonify([dict(book) for book in books])
--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask , jsonify
 import os
+import requests
 
-// Setting up CATALOG_SERVER and ORDER_SERVER for servving the catalog and order services.
+# Setting up CATALOG_SERVER and ORDER_SERVER for servving the catalog and order services.
 app = Flask(__name__)
 CATALOG_URL = os.getenv('CATALOG_SERVER', 'http://catalog:5001')
 ORDER_URL = os.getenv('ORDER_SERVER', 'http://order:5002')
 
+# Endpoint to request to get books by topic from catalog server.
+@app.route('/search/<topic>', methods=['GET'])
+def search(topic):
+    try:
+        response = requests.get(f'{CATALOG_URL}/search/{topic}', timeout=3)
+        response.raise_for_status()
+        return jsonify({'results': response.json()}), 200
+    except requests.exceptions.RequestException:
+        return jsonify({'error': 'Catalog service unavailable'}), 503
